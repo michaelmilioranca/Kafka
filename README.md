2f1844e4d5cc8dd13cb0c4c70e9ca9356d3dff98
Michael Miliorança <michael.milioranca@gmail.com>
2022-09-07 16:58:52 +0100

    Typoe changes and also generation of key

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ Commands:
 * Start Kafka:  bin/kafka-server-start config/server.properties
 * Create topic: bin/kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 partitions 1 --topic LOJA_NOVO_PEDIDO
 * List topics: bin/kafka-topics --list --bootstrap-server localhost:9092
-* Produce messages: bin/kafka-console-producer --broker-list localhost:9092 --topic LOJA_NOVO_PEDIDO ( everything you type will be a message to the topic
-* Consume messages: bin/kafka-console-consumer --bootstrap-server localhost:9092 --topic LOJA_NOVO_PEDIDO --from-beginning ( the last command will read since the beginning, if not passed It will read only new messages )
+* Produce messages: bin/kafka-console-producer --broker-list localhost:9092 --topic ECOMMERCE_NEW_ORDER ( everything you type will be a message to the topic
+* Consume messages: bin/kafka-console-consumer --bootstrap-server localhost:9092 --topic ECOMMERCE_NEW_ORDER --from-beginning ( the last command will read since the beginning, if not passed It will read only new messages )
+* Alter topic properties: bin/kafka-topics.sh --alter --zookeeper localhost:2181 --topic ECOMMERCE_NEW_ORDER --partitions 2
 
 Tips
-Todos os comandos são feitos dentro da pasta do Kafka
-A pasta em que o Kafka se encontra, não pode ter espaços se não da tilt
-O Kafka so vai funcionar se o zookeeper estiver de pé
+The Kafka folder should not have spaces in the name 
+Kafka will only work if we have a zookeeper running
+Any alteration in the config properties will affect ONLY new topics
+They way kafka decides which topic to use when it has multiples one are defined by the "key" value of the record
--- src/main/java/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/com/alura/ecommerce/NewOrderMain.java
@@ -8,6 +8,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Objects;
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 import static com.alura.ecommerce.util.PublicConstants.ECOMMERCE_TOPIC;
@@ -16,15 +17,16 @@ import static com.alura.ecommerce.util.PublicConstants.ECOMMERCE_TOPIC_SEND_EMAI
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(getKafkaProperties());
-        String message = "123467,5312156,3321.53";
+        var producer = new KafkaProducer<String, String>(getKafkaProperties());
+        String key = UUID.randomUUID().toString();
+        String message = key.concat(",5312156,3321.53");
         // For this simple test we will be using the same value for key and value
-        ProducerRecord<String, String> record = new ProducerRecord<>(ECOMMERCE_TOPIC, message, message);
+        ProducerRecord<String, String> record = new ProducerRecord<>(ECOMMERCE_TOPIC, key, message);
         // The send is assyncronous, if we want to something with the "return" of it, we have to use get
         // The send also can receive a callback, which is has de data and the exception
         producer.send(record, getDefaultCallback()).get();
         var email = "Thank you for your order! We are processing your order!";
-        var emailRecord = new ProducerRecord<>(ECOMMERCE_TOPIC_SEND_EMAIL, email, email);
+        var emailRecord = new ProducerRecord<>(ECOMMERCE_TOPIC_SEND_EMAIL, key, email);
         producer.send(emailRecord,getDefaultCallback()).get();
     }
 
